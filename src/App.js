9f118d341b2fdb036216171bc2bebd1ad027abb2
Zac Adams <77133479+zacherya@users.noreply.github.com>
2022-05-06 14:41:11 +1000

    Structure project filesystem / inital design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
